01a9a8f13148201964593749de32b7272e1a9ef6
emil41m5 <emil41m5@CV-BB-5340>
2021-08-24 10:58:09 +0200

    Add project files.

---
+++ EX14Lists/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace EX14Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> ages = new List<int>();
+
+            ages.Add(5);
+            ages.Add(17);
+            ages.Add(20);
+            ages.Add(40);
+            ages.Add(51);
+
+            List<string> names = new List<string>();
+
+            names.Add("Jens");
+            names.Add("Karl");
+            names.Add("Johannes");
+            names.Add("Anton");
+            names.Add("Nicklas");
+
+            List<double> percentages = new List<double>() {0.75, 0.23, 0.86, 0.17};
+
+            List<bool> areMarried = new List<bool>() {true, false, false, true, true};
+
+            for (int i = 0; i < ages.Count; i++)
+            {
+                Console.WriteLine(ages[i]);
+            }
+
+            for (int i = 0; i < names.Count; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+
+            foreach (double percentage in percentages)
+            {
+                Console.WriteLine(percentage);
+            }
+
+            foreach (bool isMarried in areMarried)
+            {
+                Console.WriteLine(isMarried);
+            }
+        }
+    }
+}
